5557d1ab37da26aaf73eab5c74b52bdb2c5bdda7
albertoBarl <alberto.barletta00@gmail.com>
2022-12-19 17:35:56 +0100

    added autoplay and click-on-thumbnails

--- js/script.js
+++ js/script.js
@@ -34,14 +34,28 @@ createApp({
     };
   },
   methods: {
+    switchImg(index) {
+      this.activeImage = index;
+    },
     next() {
       this.activeImage++;
-      if (this.activeImage === this.slides.lenght - 1) {
+      if (this.activeImage === this.slides.length) {
         this.activeImage = 0;
       }
     },
     prev() {
       this.activeImage--;
+      if (this.activeImage < 0) {
+        this.activeImage = this.slides.length - 1;
+      }
+    },
+    autoNext() {
+      this.autoPlay = setInterval(() => {
+        this.next();
+      }, 3000);
+    },
+    autoStop() {
+      clearInterval(this.autoPlay);
     },
   },
 }).mount("#app");
